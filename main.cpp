7b309cf4060f9e2c2094491a2ca78778200358d8
Emilis Baliukonis <emilisb@wix.com>
2019-02-11 12:29:50 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,65 @@
+//
+//  main.cpp
+//  Uzduotis 1
+//
+//  Created by Emilis Baliukonis on 11/02/2019.
+//  Copyright © 2019 Emilis Baliukonis. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::cin;
+using std::string;
+using std::endl;
+
+string getBorder(int width) {
+    string border = "";
+    
+    for (int i = 0; i < width; i++) {
+        border += "*";
+    }
+    
+    return border;
+}
+
+string getVerticalBorder(int width) {
+    string border = "*";
+    
+    for (int i = 0; i < width - 2; i++) {
+        border += " ";
+    }
+    
+    border += "*";
+    
+    return border;
+}
+
+string getWelcomeLine(string text) {
+    return "* " + text + " *";
+}
+
+int main(int argc, const char * argv[]) {
+    string name;
+    
+    cout << "Įveskite vardą: ";
+    cin >> name;
+    
+    const string welcomeText = "Sveikas, " + name + "!";
+    const int width = welcomeText.length() + 4;
+    
+    const string line1 = getBorder(width);
+    const string line2 = getVerticalBorder(width);
+    const string line3 = getWelcomeLine(welcomeText);
+    const string line4 = getVerticalBorder(width);
+    const string line5 = getBorder(width);
+    
+    cout << line1 << endl;
+    cout << line2 << endl;
+    cout << line3 << endl;
+    cout << line4 << endl;
+    cout << line5 << endl;
+    
+    return 0;
+}
